b93d82aaa5e2c940ca2ce588bdf2d3bbafc251a7
cercenazi <celil.cercenazi@gmail.com>
2021-10-30 13:12:47 +0300

    add the initial spring project

---
+++ src/main/java/codeit/project/lacarta/LacartaApplication.java
@@ -0,0 +1,13 @@
+package codeit.project.lacarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LacartaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LacartaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codeit/project/lacarta/LacartaApplicationTests.java
@@ -0,0 +1,13 @@
+package codeit.project.lacarta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LacartaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
